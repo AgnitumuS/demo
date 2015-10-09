7ee8ec18037221dda572f36cd7bc489000bb4468
Stanislav Kalashnik <sk@infomir.eu>
2015-10-09 18:24:46 +0300

    migrate from stb-demo

--- app/js/stb/readme.md
+++
@@ -1,6 +0,0 @@
-## Modules
-
- Name    | Description
----------|-------------
- emitter | [publish-subscribe pattern](https://en.wikipedia.org/wiki/Publish%E2%80%93subscribe_pattern) implementation
- model   | base model implementation
--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,4 @@
 
 'use strict';
 
-var fs   = require('fs'),
-	path = require('path');
-
-
-/**
- * Load all js modules from the given directory.
- *
- * @param {string} dir folder with modules
- */
-function load ( dir ) {
-	// may be missing
-	if ( fs.existsSync(dir) ) {
-		// get file list and walk through it
-		require('fs').readdirSync(dir).forEach(function ( name ) {
-			// make correct absolute path
-			name = path.join(dir, name);
-			// check file type
-			if ( path.extname(name) === '.js' ) {
-				require(name);
-			}
-		});
-	}
-}
-
-
-// enable colors in console
-require('tty-colors');
-
-
-// general app paths
-global.paths = {
-	root:   process.env.PATH_ROOT   || __dirname,
-	tasks:  process.env.PATH_TASKS  || path.join(__dirname, 'tasks'),
-	app:    process.env.PATH_APP    || path.join(__dirname, 'app'),
-	build:  process.env.PATH_BUILD  || path.join(__dirname, 'build'),
-	config: process.env.PATH_CONFIG || path.join(__dirname, 'config')
-};
-
-
-// load tasks
-load(global.paths.tasks);
+require('stb-gulp');
